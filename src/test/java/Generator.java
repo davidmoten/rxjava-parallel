86a7b367ddd7761677ac23eecec8481594cf2ed6
Dave Moten <davidmoten@gmail.com>
2014-09-11 22:19:31 +1000

    clean

--- src/test/java/Generator.java
+++ src/test/java/Generator.java
@@ -2,8 +2,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.List;
-
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Generator {
 
@@ -11,26 +12,68 @@ public class Generator {
         InputStream is = Generator.class.getResourceAsStream("/signatures.txt");
         BufferedReader br = new BufferedReader(new InputStreamReader(is));
         String line;
-        while ((line = br.readLine())!=null) {
+        while ((line = br.readLine()) != null) {
             System.out.println(line);
+            Sig sig = from(line);
         }
         br.close();
     }
-    
+
     public static void main(String[] args) throws IOException {
         new Generator().run();
     }
-    
+
+    private static Sig from(String line) {
+
+        AtomicInteger i = new AtomicInteger(0);
+        String typeParameter = "";
+        String methodReturnType = "";
+        String methodName = "";
+        String token = "";
+        List<Parameter> parameters = new ArrayList<Parameter>();
+        while (i.get() < line.length()) {
+            char ch = line.charAt(i.get());
+            if (ch!=' ') {
+                token = readType(i, line);
+            }
+            i.incrementAndGet();
+        }
+
+        return null;
+    }
+
+    private static String readType(AtomicInteger i, String line) {
+        int level = 0;
+        while (i.get()<line.length() && (level >0 || (ch!=' ' && ch!='(' && ch!=')')){
+            
+        }
+    }
+
     private static class Sig {
         String typeParameters;
         String methodReturnType;
         String methodName;
         List<Parameter> parameters;
+
+        Sig(String typeParameters, String methodReturnType, String methodName,
+                List<Parameter> parameters) {
+            this.typeParameters = typeParameters;
+            this.methodReturnType = methodReturnType;
+            this.methodName = methodName;
+            this.parameters = parameters;
+        }
+
     }
-    
+
     private static class Parameter {
         String type;
         String name;
+
+        public Parameter(String type, String name) {
+            this.type = type;
+            this.name = name;
+        }
+
     }
-    
+
 }
