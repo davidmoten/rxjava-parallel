cc83c5e3d17a25d3602a75acc0eea7efe133b77a
Dave Moten <davidmoten@gmail.com>
2014-09-12 16:26:55 +1000

    dev

--- src/test/java/com/github/davidmoten/rx/parallel/Generator.java
+++ src/test/java/com/github/davidmoten/rx/parallel/Generator.java
@@ -18,8 +18,21 @@ public class Generator {
 
 	public void run() throws IOException, ParseException {
 
+		List<String> lines = new ArrayList<String>();
+		{
+			InputStream is = Generator.class
+					.getResourceAsStream("/signatures.txt");
+			BufferedReader br = new BufferedReader(new InputStreamReader(is));
+			String line;
+			while ((line = br.readLine()) != null) {
+				lines.add(line);
+			}
+			br.close();
+		}
+
 		// creates an input stream for the file to be parsed
-		InputStream  is = Generator.class.getResourceAsStream("/Observable.java");
+		InputStream is = Generator.class
+				.getResourceAsStream("/Observable.java");
 
 		CompilationUnit cu;
 		try {
@@ -31,170 +44,16 @@ public class Generator {
 
 		// prints the resulting compilation unit to default system output
 		System.out.println(cu.toString());
-		
-		 // visit and print the methods names
-        new MethodVisitor().visit(cu, null);
+
+		// visit and print the methods names
+		new MethodVisitor(lines).visit(cu, null);
 
 		System.exit(0);
 
-//		InputStream is = Generator.class.getResourceAsStream("/signatures.txt");
-//		BufferedReader br = new BufferedReader(new InputStreamReader(is));
-//		String line;
-//		while ((line = br.readLine()) != null) {
-//			// System.out.println(line);
-//			processSig(line);
-//		}
-//		br.close();
 	}
-
-	private static String toString(Sig sig) {
-		StringBuilder s = new StringBuilder();
-		s.append("public final ");
-		s.append(sig.typeParameters);
-		if (sig.typeParameters.length() > 0)
-			s.append(" ");
-		s.append("Parallel");
-		s.append(sig.methodReturnType);
-		s.append(" ");
-		s.append(sig.methodName);
-		s.append("(");
-		boolean first = true;
-		for (Parameter p : sig.parameters) {
-			if (!first)
-				s.append(", ");
-			s.append("final ");
-			s.append(p.type);
-			s.append(" ");
-			s.append(p.name);
-			first = false;
-
-		}
-		s.append(") {\n");
-		s.append("    return create(new Func1<Observable<T>, "
-				+ sig.methodReturnType + ">() {\n");
-		s.append("                        @Override\n");
-		s.append("                        public " + sig.methodReturnType
-				+ " call(Observable<T> o) {\n");
-		s.append("                            return o." + sig.methodName + "(");
-		first = true;
-		for (Parameter p : sig.parameters) {
-			if (!first)
-				s.append(", ");
-			s.append(p.name);
-			first = false;
-		}
-		s.append(");\n");
-		s.append("                        }\n");
-		s.append("                    });\n");
-		s.append("}");
-
-		// @Override
-		// public Observable<R> call(Observable<T> o) {
-		// return o.compose(transformer);
-		// }
-		// });
-		return s.toString();
-	}
-
-	private void processSig(String line) {
-		Sig sig = from(line);
-
-		System.out.println(toString(sig));
-	}
-
-	public static void main(String[] args) throws Exception {
+	
+	public static void main(String[] args) throws IOException, ParseException {
 		new Generator().run();
-	}
-
-	private static Sig from(String line) {
-
-		AtomicInteger i = new AtomicInteger(0);
-		AtomicBoolean inParameters = new AtomicBoolean(false);
-		String typeParameter = "";
-		String methodReturnType = "";
-		String methodName = "";
-		String token = "";
-		String paramType = "";
-		List<Parameter> parameters = new ArrayList<Parameter>();
-
-		while (i.get() < line.length()) {
-			char ch = line.charAt(i.get());
-			if (ch != ' ') {
-				token = readToken(i, inParameters, line);
-				if (token.startsWith("<"))
-					typeParameter = token;
-				else if (methodReturnType.length() == 0
-						&& !token.startsWith("<"))
-					methodReturnType = token;
-				else if (methodReturnType.length() > 0
-						&& methodName.length() == 0)
-					methodName = token;
-				else if (inParameters.get()) {
-					if (paramType.length() == 0)
-						paramType = token;
-					else {
-						parameters.add(new Parameter(paramType, token));
-						paramType = "";
-					}
-				}
-			} else
-				i.incrementAndGet();
-		}
-
-		return new Sig(typeParameter, methodReturnType, methodName, parameters);
-	}
-
-	private static String readToken(AtomicInteger i,
-			AtomicBoolean inParameters, String line) {
-		int level = 0;
-		char ch = line.charAt(i.get());
-		StringBuilder s = new StringBuilder();
-		while (i.get() < line.length()
-				&& (level > 0 || (ch != ' ' && ch != ',' && ch != '(' && ch != ')'))) {
-			if (ch == '<')
-				level++;
-			if (ch == '>')
-				level--;
-			s.append(ch);
-			i.incrementAndGet();
-			if (i.get() < line.length())
-				ch = line.charAt(i.get());
-		}
-		if (ch == '(') {
-			inParameters.set(true);
-			i.incrementAndGet();
-		} else if (ch == ')')
-			i.incrementAndGet();
-		else if (ch == ',')
-			i.incrementAndGet();
-		return s.toString();
-	}
-
-	private static class Sig {
-		String typeParameters;
-		String methodReturnType;
-		String methodName;
-		List<Parameter> parameters;
-
-		Sig(String typeParameters, String methodReturnType, String methodName,
-				List<Parameter> parameters) {
-			this.typeParameters = typeParameters;
-			this.methodReturnType = methodReturnType;
-			this.methodName = methodName;
-			this.parameters = parameters;
-		}
-
-	}
-
-	private static class Parameter {
-		String type;
-		String name;
-
-		public Parameter(String type, String name) {
-			this.type = type;
-			this.name = name;
-		}
-
 	}
 
 }
--- src/test/java/com/github/davidmoten/rx/parallel/MethodVisitor.java
+++ src/test/java/com/github/davidmoten/rx/parallel/MethodVisitor.java
@@ -1,42 +1,77 @@
 package com.github.davidmoten.rx.parallel;
 
+import japa.parser.ast.Node;
 import japa.parser.ast.TypeParameter;
 import japa.parser.ast.body.MethodDeclaration;
+import japa.parser.ast.expr.AnnotationExpr;
 import japa.parser.ast.type.ClassOrInterfaceType;
 import japa.parser.ast.visitor.VoidVisitorAdapter;
 
-public class MethodVisitor extends VoidVisitorAdapter {
+import java.util.List;
+
+public class MethodVisitor extends VoidVisitorAdapter<Object> {
+
+	private List<String> lines;
+
+	public MethodVisitor(List<String> lines) {
+		this.lines = lines;
+	}
 
 	@Override
 	public void visit(MethodDeclaration m, Object arg) {
 		// here you can access the attributes of the method.
 		// this method will be called for all methods in this
 		// CompilationUnit, including inner class methods
-		System.out.println(m.getName());
 		StringBuilder s = new StringBuilder();
+		
+		//type parameter section
+		if (m.getAnnotations()!=null && !m.getAnnotations().isEmpty()) {
+			AnnotationExpr a = m.getAnnotations().get(0);
+		}
 		if (m.getTypeParameters() != null && !m.getTypeParameters().isEmpty()) {
 			s.append("<");
 			boolean first = true;
 			for (TypeParameter t : m.getTypeParameters()) {
 				if (!first)
 					s.append(", ");
-				s.append(t.getName());
-				if (t.getTypeBound() != null) {
-					boolean firstBound = true;
-					for (ClassOrInterfaceType bound : t.getTypeBound()) {
-						if (firstBound)
-							s.append(" extends ");
-						else
-							s.append(" & ");
-						s.append(bound.getName());
-						firstBound = false;
-					}
-				}
+				appendNode(s,t);
+//				s.append(t.getName());
+//				if (t.getTypeBound() != null) {
+//					boolean firstBound = true;
+//					for (ClassOrInterfaceType bound : t.getTypeBound()) {
+//						if (firstBound)
+//							s.append(" extends ");
+//						else
+//							s.append(" & ");
+//						s.append(bound.getName());
+//						firstBound = false;
+//					}
+//				}
 				first = false;
 			}
 			s.append(">");
 		}
+		
+		//method return
+		
 		System.out.println(s);
+	}
+
+	private void appendNode(StringBuilder s, Node node) {
+		for (int lineNum = node.getBeginLine(); lineNum <= node.getEndLine(); lineNum++) {
+			String line = lines.get(lineNum);
+			int start;
+			if (lineNum == node.getBeginLine())
+				start = node.getBeginColumn();
+			else
+				start = 0;
+			int end;
+			if (lineNum == node.getEndLine())
+				end = node.getEndColumn();
+			else
+				end = line.length();
+			s.append(line.substring(start, end));
+		}
 	}
 
 }
