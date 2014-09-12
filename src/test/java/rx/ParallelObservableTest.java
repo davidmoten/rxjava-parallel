0801dd87ccad689207437a36b2d9b905467803bd
Dave Moten <davidmoten@gmail.com>
2014-09-12 17:57:53 +1000

    get working

--- src/test/java/rx/ParallelObservableTest.java
+++ src/test/java/rx/ParallelObservableTest.java
@@ -5,12 +5,14 @@ import static rx.ParallelObservable.parallel;
 
 import org.junit.Test;
 
+import rx.functions.Functions;
+
 public class ParallelObservableTest {
 
 	@Test
 	public void test() {
-		assertEquals(1000,(int) parallel(Observable.range(1, 1000))
+		assertEquals(1000,(int) parallel(Observable.range(1, 1000)).map(Functions.identity()).buffer(2,1)
 				.flatten().count().toBlocking().single());
 	}
-
+	
 }
