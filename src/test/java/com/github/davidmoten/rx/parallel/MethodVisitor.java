d75cbe3262288482c9313dadbc9f80828c6bd6e9
Dave Moten <davidmoten@gmail.com>
2014-09-12 16:54:32 +1000

    dev

--- src/test/java/com/github/davidmoten/rx/parallel/MethodVisitor.java
+++ src/test/java/com/github/davidmoten/rx/parallel/MethodVisitor.java
@@ -3,10 +3,11 @@ package com.github.davidmoten.rx.parallel;
 import japa.parser.ast.Node;
 import japa.parser.ast.TypeParameter;
 import japa.parser.ast.body.MethodDeclaration;
-import japa.parser.ast.expr.AnnotationExpr;
+import japa.parser.ast.body.Parameter;
 import japa.parser.ast.type.ClassOrInterfaceType;
 import japa.parser.ast.visitor.VoidVisitorAdapter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MethodVisitor extends VoidVisitorAdapter<Object> {
@@ -19,44 +20,75 @@ public class MethodVisitor extends VoidVisitorAdapter<Object> {
 
 	@Override
 	public void visit(MethodDeclaration m, Object arg) {
+		if (!m.getType().toString().startsWith("Observable"))
+			return;
 		// here you can access the attributes of the method.
 		// this method will be called for all methods in this
 		// CompilationUnit, including inner class methods
 		StringBuilder s = new StringBuilder();
 		
 		//type parameter section
-		if (m.getAnnotations()!=null && !m.getAnnotations().isEmpty()) {
-			AnnotationExpr a = m.getAnnotations().get(0);
-		}
 		if (m.getTypeParameters() != null && !m.getTypeParameters().isEmpty()) {
 			s.append("<");
 			boolean first = true;
 			for (TypeParameter t : m.getTypeParameters()) {
 				if (!first)
 					s.append(", ");
-				appendNode(s,t);
-//				s.append(t.getName());
-//				if (t.getTypeBound() != null) {
-//					boolean firstBound = true;
-//					for (ClassOrInterfaceType bound : t.getTypeBound()) {
-//						if (firstBound)
-//							s.append(" extends ");
-//						else
-//							s.append(" & ");
-//						s.append(bound.getName());
-//						firstBound = false;
-//					}
-//				}
+				s.append(t.getName());
+				if (t.getTypeBound() != null) {
+					boolean firstBound = true;
+					for (ClassOrInterfaceType bound : t.getTypeBound()) {
+						if (firstBound)
+							s.append(" extends ");
+						else
+							s.append(" & ");
+						s.append(bound.getName());
+						firstBound = false;
+					}
+				}
 				first = false;
 			}
 			s.append(">");
 		}
+		String typeParameters = s.toString();
 		
 		//method return
+		String methodReturnType = m.getType().toString();
 		
-		System.out.println(s);
+		//parameters
+		List<MyParameter> parameters = new ArrayList<MyParameter>();
+		if (m.getParameters()!=null && !m.getParameters().isEmpty()) {
+			for (Parameter p:m.getParameters()) {
+				parameters.add(new MyParameter(p.getType().toString(), p.getId().getName()));
+			}
+		}
+		
+		StringBuilder r = new StringBuilder();
+		
+		r.append("public final " + typeParameters + " " + methodReturnType + " " + m.getName() + "(");
+		boolean first = true;
+		for (MyParameter p:parameters) {
+			if (!first)
+				r.append(", ");
+			r.append(p.type + " " + p.name);
+		}
+		r.append(") {\n");
+		r.append("    return create(new Func1<Observable<T>,"+methodReturnType + ">() {});\n" );
+		System.out.println(r);
 	}
 
+	
+	private static class MyParameter {
+		String type;
+		String name;
+
+		public MyParameter(String type, String name) {
+			this.type = type;
+			this.name = name;
+		}
+		
+	}
+	
 	private void appendNode(StringBuilder s, Node node) {
 		for (int lineNum = node.getBeginLine(); lineNum <= node.getEndLine(); lineNum++) {
 			String line = lines.get(lineNum);
